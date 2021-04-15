26d1ace7218f8560d66d9fe2e7396132013a9196
Junhong0209 <junh040209@naver.com>
2021-04-15 09:49:56 +0900

    Code Repectoring
    원래 코드가 매우 더러워서 파이썬의 클래스와 함수를 배워 새로 만들었습니다.
    
    - Python 파일 안에 있는 것 -
    main.py - Discord Bot 명령어
    config.py - token, Embed Color, date
    utils.py - Class, function ...
    database.py - School Logo

---
+++ database.py
@@ -0,0 +1,6 @@
+DGSW_Logo = "https://media.discordapp.net/attachments/819001182369611807/830064995218751488/20cf1df326f8ec26.png?width=1144&height=702"
+YALE_Logo = "https://media.discordapp.net/attachments/819001182369611807/830065004627230750/2f48c644fc26fbef.png?width=690&height=702"
+MOONHWA_Logo = "https://media.discordapp.net/attachments/819001182369611807/830065000038268958/afbe18eb159ba960.png?width=693&height=701"
+DONGSUNG_Logo = "https://media.discordapp.net/attachments/819001182369611807/830066751667830794/f331f5b70a79498e.png?width=893&height=702"
+SILLA_TACHNICAL_Logo = "https://media.discordapp.net/attachments/819001182369611807/830064998650085436/2552855f4a26158d.png?width=846&height=702"
+POHANG_JECHEOL_TACHNICAL_Logo = "https://media.discordapp.net/attachments/819001182369611807/830065000013103104/7bc6a09a1f12816a.png?width=556&height=701"
---
+++ main.py
@@ -0,0 +1,288 @@
+import discord
+import asyncio
+
+import config
+import utils
+import database
+
+from discord.ext import commands
+
+token = config.Config.token
+
+app = commands.Bot(command_prefix='!')
+
+Color = config.Config.Color
+HyperScape_Color = config.Config.HyperScape_Color
+
+today = config.Config.today
+tomorrow = config.Config.tomorrow
+
+DGSW_Logo = database.DGSW_Logo
+YALE_Logo = database.YALE_Logo
+MONNHWA_Logo = database.MOONHWA_Logo
+DONSUNG_Logo = database.DONGSUNG_Logo
+SILLA_TACHNICAL_Logo = database.SILLA_TACHNICAL_Logo
+POHANG_JECHEOL_TACHNICAL_Logo = database.POHANG_JECHEOL_TACHNICAL_Logo
+
+@app.event
+async def on_ready():
+    print('Loggend-in Bot:', app.user.name)
+    print('Bot id:', app.user.id)
+    print('connection was succesful')
+    print('=' * 30)
+
+    game = discord.Game('정신 차리기')
+    await app.change_presence(status=discord.Status.offline, activity=game)
+    await asyncio.sleep(5)
+    while True:
+        game = discord.Game('빡추 스탯 쌓기')
+        await app.change_presence(status=discord.Status.online, activity=game)
+        await asyncio.sleep(3)
+        game = discord.Game('여전히 베타 테스트 중이에요~')
+        await app.change_presence(status=discord.Status.online, activity=game)
+        await asyncio.sleep(3)
+        game = discord.Game('테스트가 언제 쯤 끝날까요~')
+        await app.change_presence(status=discord.Status.online, activity=game)
+        await asyncio.sleep(3)
+        game = discord.Game('!도움말')
+        await app.change_presence(status=discord.Status.online, activity=game)
+        await asyncio.sleep(3)
+
+#################### 도움말 ####################
+
+@app.command()
+async def 도움말(ctx):
+    embed = discord.Embed(title="명령어 모음", description="", colour=Color)
+    embed.add_field(name="!안녕", value="봇이 인사를 함", inline=False)
+    embed.add_field(name="!빡추 [이름]", value="들어간 이름이 빡추 스탯을 쌓음", inline=False)
+    embed.add_field(name="!초대링크", value="봇 초대 링크를 보내줌", inline=False)
+    embed.add_field(name="!핲스 도움말", value="하이퍼 스케이프 전적 검색에 사용되는 명령어를 알려준다.", inline=False)
+    embed.add_field(name="!씹덕 [이름]", value="말 그대로 씹덕.....", inline=False)
+    embed.add_field(name="!급식", value="급식과 관련된 도움말 알려줌", inline=False)
+    embed.set_footer(text='made by 빨강고양이', icon_url='https://cdn.discordapp.com/attachments/819001182369611807/819001250850668605/9965e852f4552224.png')
+    await ctx.send(embed=embed)
+
+# @app.command(aliases=['핲스'])
+# async def HyperScape_help(ctx, *, text):
+#     if text == '도움말':
+#         embed = discord.Embed(title="명령어 사용방법!", color=HyperScape_Color)
+#         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/708693776180314223/743432880969220156/Untitled-1.png")
+#         embed.add_field(name="PC용 킬뎃 확인", value="!pc [닉네임]", inline=True)
+#         embed.add_field(name="PS4용 킬뎃 확인", value="!ps4 [닉네임]", inline=True)
+#         embed.add_field(name="XBOX용 킬뎃 확인", value="!xbox [닉네임]", inline=True)
+#         embed.set_footer(text='serviced by hyper scape korea', icon_url='https://media.discordapp.net/attachments/708693776180314223/731835374619328619/HS.png?width=684&height=684')
+#         await ctx.send(embed=embed)
+#     else :
+#         await ctx.send(embed=utils.Error())
+
+@app.command()
+async def 급식(ctx):
+    embed = discord.Embed(title='급식 도움말', color=Color)
+    embed.add_field(name="!대소고 급식", value="대소고 하루 급식을 보여준다.", inline=False)
+    embed.add_field(name="!문화고 급식", value="문화고 하루 급식을 보여준다.", inline=False)
+    embed.add_field(name="!예일고 급식", value="예일고 하루 급식을 보여준다.", inline=False)
+    embed.add_field(name="!신라공고 급식", value="신라공고 하루 급식을 보여준다.", inline=False)
+    embed.add_field(name="!동성고 급식", value="동성고 하루 급식을 보여준다.", inline=False)
+    embed.add_field(name="!포철공고 급식", value="포철공고 하루 급식을 보여준다.", inline=False)
+    embed.add_field(name="![학교 이름] 급식", value="추후 다른 고등학교 추가 예정", inline=False)
+    embed.set_footer(text='made by 빨강고양이', icon_url='https://cdn.discordapp.com/attachments/819001182369611807/819001250850668605/9965e852f4552224.png')
+    await ctx.send(embed=embed)
+
+#################### 급식 조회 ####################
+
+@app.command()
+async def 대소고(ctx, *, schoolMeal):
+    if schoolMeal == '급식':
+        Embed = utils.getMeal_Today({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'D10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '7240454',  # 표준학교코드
+            'MLSV_YMD': today  # 급식일자
+        }
+        , DGSW_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    elif schoolMeal == '내일 급식' or '내일급식':
+        Embed = utils.getMeal_Tomorrow({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'D10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '7240454',  # 표준학교코드
+            'MLSV_YMD': tomorrow  # 급식일자
+        }
+        , DGSW_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    else:
+        await ctx.send(embed=utils.Error())
+
+@app.command()
+async def 문화고(ctx, *, schoolMeal):
+    if schoolMeal == '급식':
+        Embed = utils.getMeal_Today({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750172',  # 표준학교코드
+            'MLSV_YMD': today  # 급식일자
+        }
+        , MONNHWA_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    elif schoolMeal == '내일 급식' or '내일급식':
+        Embed = utils.getMeal_Tomorrow({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750172',  # 표준학교코드
+            'MLSV_YMD': tomorrow  # 급식일자
+        }
+        , MONNHWA_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    else:
+        await ctx.send(embed=utils.Error())
+
+@app.command()
+async def 예일고(ctx, *, schoolMeal):
+    if schoolMeal == '급식':
+        Embed = utils.getMeal_Today({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750772',  # 표준학교코드
+            'MLSV_YMD': today  # 급식일자
+        }
+            , YALE_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    elif schoolMeal == '내일 급식' or '내일급식':
+        Embed = utils.getMeal_Tomorrow({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750772',  # 표준학교코드
+            'MLSV_YMD': tomorrow  # 급식일자
+        }
+            , YALE_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    else:
+        await ctx.send(embed=utils.Error())
+
+@app.command()
+async def 신라공고(ctx, *, schoolMeal):
+    if schoolMeal == '급식':
+        Embed = utils.getMeal_Today({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750323',  # 표준학교코드
+            'MLSV_YMD': today  # 급식일자
+        }
+            , SILLA_TACHNICAL_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    elif schoolMeal == '내일 급식' or '내일급식':
+        Embed = utils.getMeal_Tomorrow({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750323',  # 표준학교코드
+            'MLSV_YMD': tomorrow  # 급식일자
+        }
+            , SILLA_TACHNICAL_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    else:
+        await ctx.send(embed=utils.Error())
+
+@app.command()
+async def 동성고(ctx, *, schoolMeal):
+    if schoolMeal == '급식':
+        Embed = utils.getMeal_Today({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750542',  # 표준학교코드
+            'MLSV_YMD': today  # 급식일자
+        }
+            , DONSUNG_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    elif schoolMeal == '내일 급식' or '내일급식':
+        Embed = utils.getMeal_Tomorrow({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750542',  # 표준학교코드
+            'MLSV_YMD': tomorrow  # 급식일자
+        }
+            , DONSUNG_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    else:
+        await ctx.send(embed=utils.Error())
+
+@app.command()
+async def 포철공고(ctx, *, schoolMeal):
+    if schoolMeal == '급식':
+        Embed = utils.getMeal_Today({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750337',  # 표준학교코드
+            'MLSV_YMD': today  # 급식일자
+        }
+        , POHANG_JECHEOL_TACHNICAL_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    elif schoolMeal == '내일 급식' or '내일급식':
+        Embed = utils.getMeal_Tomorrow({
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': 'R10',  # 시도교육청코드
+            'SD_SCHUL_CODE': '8750337',  # 표준학교코드
+            'MLSV_YMD': tomorrow  # 급식일자
+        }
+        , POHANG_JECHEOL_TACHNICAL_Logo)
+        await ctx.send(embed=Embed.embed)
+
+    else:
+        await ctx.send(embed=utils.Error())
+
+#################### 명령어 ####################
+
+@app.command()
+async def 제작자(ctx): # 자신의 정보를 넣으면 된다.
+    embed = discord.Embed(title="제작자 정보", color=Color)
+    embed.add_field(name="Discord", value="빨강고양이#5278", inline=False)
+    embed.add_field(name="GitHub", value="[제작자의 GitHub](https://github.com/Junhong0209)", inline=False)
+    embed.add_field(name="Facebook", value="[제작자의 Facebook](https://www.facebook.com/Junhong04/)", inline=False)
+    embed.add_field(name="Instargram", value="[제작자의 Instargram](https://www.instagram.com/junhong936/)", inline=False)
+    embed.add_field(name="Blog", value="[제작자의 Blog](https://junhong0209.github.io)", inline=False)
+    embed.set_footer(text='made by 빨강고양이', icon_url='https://cdn.discordapp.com/attachments/819001182369611807/819001250850668605/9965e852f4552224.png')
+    await ctx.send(embed=embed)
+
+@app.command(aliases=['안녕', '안녕하세요', 'ㅎㅇ'])
+async def Hello(ctx):
+    await ctx.send('안녕하세요~! {0.author.mention}님. 오늘도 좋은 하루 보내세요!'.format(ctx))
+
+@app.command()
+async def 초대링크(ctx):
+    embed = discord.Embed(title="봇 초대 링크", description="", color=Color)
+    embed.add_field(name="이 봇을 다른 서버에 초대하기 위한 링크입니다.",
+                    value="[봇 초대하기](https://discord.com/api/oauth2/authorize?client_id=793085952254803988&permissions=8&scope=bot)",
+                    inline=False)
+    await ctx.send(embed=embed)
+
+@app.command()
+async def 빡추(ctx, *, text=None):
+    if None == text:       # !빡추 명령어 뒤에 아무것도 입력 하지 않은 경우
+        await ctx.send("누구를 입력하신거죠? 전 입력 받은게 없습니다만?")
+    elif text == "아서":    # !빡추 명령어 뒤에 봇의 이름을 넣은 경우
+        await ctx.send("나 빡추 아닌데?")
+    else:
+        await ctx.send("보셨나요? 보셨나요? 보셨냐구요!!!! " + text + "의 빡추 스탯쌓기!!")
+        print(text)
+
+@app.command()
+async def 씹덕(ctx, text=None):
+    if None == text:       # !씹덕 명령어 뒤에 아무것도 입력 하지 않은 경우
+        await ctx.send("누구를 입력하신거죠? 전 입력 받은게 없습니다만?")
+    elif text == "아서":    # !씹덕 명령어 뒤에 봇의 이름을 넣은 경우
+        await ctx.send("나 씹덕 아닌데?")
+    else:
+        await ctx.send("아, " + text + " 그 씹덕 샛기?")
+        print(text)
+
+app.run(token)
