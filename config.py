712d6dcd5ed5992a85e365de9d6344cbcff56c07
Junhong0209 <junh040209@naver.com>
2021-04-24 00:31:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 class Config:
-    token = "NzkzMDg1OTUyMjU0ODAzOTg4.X-nI2Q.QsGUVKfupP8VnHxBfxZ-4IdAEzw"
+    token = "your bot token"
 
     Color = 0x2EFEF7               # 이 봇의 기본 임베드 색상
     HyperScape_Color = 0x9ed7d0    # 핲스 전적 검색 임베드 색상
