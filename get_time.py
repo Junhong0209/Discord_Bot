e5759efce1a05722c14fc61d76d2f27b2185c6f6
Junhong0209 <junh040209@naver.com>
2021-05-10 16:27:37 +0900

    [ADD] - get_time.py 추가
    시간 가져오는 것을 함수로 만들어 함수를 호출하면 그때 그떄 시간을 가져오도록 만들었다.

---
+++ get_time.py
@@ -0,0 +1,20 @@
+import time
+
+from datetime import datetime, timedelta
+
+def get_time_today():
+    date = datetime.today()                   # 오늘 날짜 받아오기 (yyyy-mm-dd HH:mm:ss)
+    today = date.strftime("%y%m%d")           # date를 yyyymmdd 형태로 바꾸어줌
+    time.sleep(1)
+    return today
+    
+def get_time_tomorrow():
+    date = datetime.today() + timedelta(1)    # 내일 날짜 받아오기 (yyyy-mm-dd HH:mm:ss)
+    tomorrow = date.strftime('%y%m%d')        # date를 yyyymmdd 형태로 바꾸어줌
+    time.sleep(1)
+    return tomorrow
+    
+def get_time_now():
+    date = datetime.today()
+    time.sleep(1)
+    return date
