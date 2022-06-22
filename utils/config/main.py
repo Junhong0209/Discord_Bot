69371c2c417a20d1836c913187448e358596b0c5
Junhong0209 <junh040209@naver.com>
2022-06-22 12:09:53 +0900

    :art: :: class 위치 변경

--- utils/config/main.py
+++ utils/config/main.py
@@ -25,13 +25,5 @@ class BS4:
   Cookies = ''
 
 
-class ColorPalette:
-  main_color = 0x2EFEF7
-  error_color = 0xFF0000
-  hyperScape_color = 0x9ED7D0
-  overwatch_color = 0xF89E1B
-  league_of_legend_color = 0x5CD1E5
-
-
 class Config:
   footer_msg = 'Bot made by. 빨강고양이#5278'
